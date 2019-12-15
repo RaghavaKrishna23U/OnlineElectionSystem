9d560d10a64d6ef46e36097ff7fed1674363c808
RaghavaKrishna23U <raghavr419@gmail.com>
2019-12-15 09:32:11 +0530

    Project barebones

---
+++ src/main/java/project/main/OnlineElectionSystemApplication.java
@@ -0,0 +1,13 @@
+package project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineElectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineElectionSystemApplication.class, args);
+	}
+
+}
